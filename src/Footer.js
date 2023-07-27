57373ff531acb64e92781f992876b932113d6812
Christopher San Diego <csandiego@gmail.com>
2023-07-28 05:37:02 +0800

    Update HTML.

---
+++ src/Footer.js
@@ -0,0 +1,39 @@
+export default function Footer() {
+    return (
+        <footer>
+            <section>
+                <p>Doormat Navigation</p>
+                <nav>
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/">About</a></li>
+                        <li><a href="/">Menu</a></li>
+                        <li><a href="/">Reservations</a></li>
+                        <li><a href="/">Order Online</a></li>
+                        <li><a href="/">Login</a></li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <p>Contact</p>
+                <nav>
+                    <ul>
+                        <li><a href="/">Address</a></li>
+                        <li><a href="/">Phone number</a></li>
+                        <li><a href="/">Email</a></li>
+                    </ul>
+                </nav>
+            </section>
+            <section>
+                <p>Social Media Links</p>
+                <nav>
+                    <ul>
+                        <li><a href="/">Address</a></li>
+                        <li><a href="/">Phone number</a></li>
+                        <li><a href="/">Email</a></li>
+                    </ul>
+                </nav>
+            </section>
+        </footer>
+    );
+}
---
+++ src/Header.js
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <img src="Logo.svg" alt="Little Lemon" />
+        </header>
+    );
+}
